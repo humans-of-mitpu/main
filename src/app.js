99441a735bd60297568f469a60ca33cc377a73f6
Nara Bayarsaikhan <nbayarsa@us.ibm.com>
2018-10-02 17:17:51 -0700

    add angular

---
+++ src/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('mitpuApp', [
+  'ngRoute',
+  'mitpuApp.playlist'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/playlist'});
+}]);
+
+angular.module('mitpuApp.playlist', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/playlist', {
+    templateUrl: 'playlist.html',
+    controller: 'mainCtrl'
+  });
+}])
+
+.controller('mainCtrl', [function() {
+
+}]);
